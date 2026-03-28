68b3df5f46a0cedf4e49e8c26d2acae0af9da590
Te0866 <johann_te@dlsu.edu.ph>
2026-03-28 19:07:38 +0800

    Update edituserdata.php

--- edituserdata.php
+++ edituserdata.php
@@ -2,33 +2,36 @@
 require "dbconnection.php";
 mysqli_select_db($con, "apsdb");
 
+$row = null; 
+$id = null;
+$updates = [];
 
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
+$row = null;
 
-    $result = $con->query("SELECT * FROM users WHERE user_id='$id'");
-    $row = $result->fetch_assoc();
-}
-
-if(isset($_POST['update'])){
+if (isset($_POST['update'])) {
 
     $id = $_POST['id'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $role = $_POST['role'];
-    $status = $_POST['status'];
+    $updates = [];
 
-    $sql = "UPDATE users 
-            SET username='$username',
-                password='$password',
-                role='$role',
-                status='$status'
-            WHERE user_id='$id'";
+    if (!empty($_POST['username'])) {
+        $updates[] = "username='" . $_POST['username'] . "'";
+    }
 
-    if($con->query($sql) === TRUE){
-        echo "User updated successfully";
-    } else {
-        echo "Error updating user: " . $con->error;
+    if (!empty($_POST['password'])) {
+        $updates[] = "password='" . $_POST['password'] . "'";
+    }
+
+    if (!empty($_POST['role'])) {
+        $updates[] = "role='" . $_POST['role'] . "'";
+    }
+
+    if (isset($_POST['status'])) {
+        $updates[] = "status='" . $_POST['status'] . "'";
+    }
+
+    if (!empty($updates) && !empty($id)) {
+        $sql = "UPDATE users SET " . implode(", ", $updates) . " WHERE user_id='$id'";
+        $con->query($sql);
     }
 }
 ?>
@@ -42,26 +45,27 @@ if(isset($_POST['update'])){
 
 <h2>Edit User</h2>
 
+
 <form method="POST">
-
-<input type="hidden" name="id" value="<?php echo $row['user_id']; ?>">
-
+ID number:<br>
+<input type="text" name="id" >
+<br><br>
 Username:<br>
-<input type="text" name="username" value="<?php echo $row['username']; ?>" required>
+<input type="text" name="username" >
 <br><br>
 
 Password:<br>
-<input type="text" name="password" value="<?php echo $row['password']; ?>" required>
+<input type="text" name="password"  >
 <br><br>
 
 Role:<br>
-<input type="text" name="role" value="<?php echo $row['role']; ?>" required>
+<input type="text" name="role" >
 <br><br>
 
 Status:<br>
 <select name="status">
-    <option value="1" <?php if($row['status']==1) echo "selected"; ?>>Active</option>
-    <option value="0" <?php if($row['status']==0) echo "selected"; ?>>Inactive</option>
+    <option value="1" >Active</option>
+    <option value="0" >Inactive</option>
 </select>
 
 <br><br>
@@ -69,6 +73,8 @@ Status:<br>
 <input type="submit" name="update" value="Update User">
 
 </form>
+
+
 
 <br>
 <a href="admin.php">Back</a>
