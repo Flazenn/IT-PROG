ef7afe35b3ec42c7671fae0a619a482154ec2e3a
leftythebestcoder <phil_tan@dlsu.edu.ph>
2026-03-29 20:52:29 +0800

    Change login redirection to login.php
    Updated login redirection to point to login.php instead of qwerty.php.

--- auth.php
+++ auth.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // Check if user is logged in
 if(!isset($_SESSION['user_id'])) {
-    header("Location: ../qwerty.php");
+    header("Location: login.php");
     exit();
 }
 
